4c723d3bb6b9ac480144f93bcea37782d50f0a63
Belaptow <gasmirnov99@gmail.com>
2024-05-13 22:16:37 +0300

    Add project files.

---
+++ IndustrialSafety/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using IndustrialSafetyLib.CoreEntities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IndustrialSafety.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ IndustrialSafetyLib/CoreEntities/Order.cs
@@ -0,0 +1,17 @@
+﻿using IndustrialSafetyLib.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndustrialSafetyLib.CoreEntities
+{
+    public class Order : Entity
+    {
+        public int CustomerID { get; set; }
+        public int EmployeeID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
