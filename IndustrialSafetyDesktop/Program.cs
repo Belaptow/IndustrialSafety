28220f93ebf690f396b370185e6b07e2c705f003
Belaptow <gasmirnov99@gmail.com>
2024-06-10 22:29:48 +0300

    Desktop wrapper

---
+++ IndustrialSafetyDesktop/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Process core = null;
+Process client = null;
+try
+{
+    core = Process.Start("IndustrialSafety.exe");
+    client = Process.Start("IndustrialSafetyWrapper-win32-x64\\IndustrialSafetyWrapper.exe");
+    client.WaitForExit();
+}
+catch(Exception ex)
+{
+    
+}
+finally
+{
+    if (core != null) core.Kill();
+    if (client != null) client.Kill();
+}
